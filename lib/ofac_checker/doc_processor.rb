27a36aacff930ef0d48dc84c91f381215e5ff47c
Codemis <johnathan@still-water.com>
2012-03-19 13:11:45 -0600

    Renamed spec helper file.

--- lib/ofac_checker/doc_processor.rb
+++ lib/ofac_checker/doc_processor.rb
@@ -1,13 +1,15 @@
 # A class for processing an ACH file, and comparing the payees with the OFAC database
 #
 class DocProcessor
+	# grant access to class attributes
+	#
+	attr_reader :doc, :result_file
 	
 	# initialize the document processor class
 	#
 	def initialize(doc, result_location)
 		@doc = doc
 		@result_file = File.join(result_location, "#{File.basename(@doc, '.*')}_complete.csv")
-		puts @result_file
 	end
 	
 end
--- spec/ofac_checker/doc_processor_spec.rb
+++ spec/ofac_checker/doc_processor_spec.rb
@@ -1,4 +1,16 @@
-describe DocProcessor
+require 'spec_helper'
 
+describe DocProcessor do
+
+	describe "#initialize" do
+		before(:each) do
+			@doc_processor = DocProcessor.new('ach_example.ach', 'spec/files/ach_example.ach')
+		end
+		
+		it "should set the doc file" do
+			@doc_processor.doc.nil?.should === false
+		end
+		
+	end
 	
 end
